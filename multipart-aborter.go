0fa00e7afe2a7a4ebdd61f2adc9b05ec52dfdf19
Jamie Stackhouse <jamie.stackhouse@redspace.com>
2015-10-26 10:40:45 -0300

    Update to aws sdk.

--- multipart-aborter.go
+++ multipart-aborter.go
@@ -3,45 +3,83 @@ package main
 import (
 	"flag"
 	"fmt"
-	"log"
 
-	"github.com/awslabs/aws-sdk-go/aws"
-	"github.com/awslabs/aws-sdk-go/aws/awsutil"
-	"github.com/awslabs/aws-sdk-go/service/s3"
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/awserr"
+	"github.com/aws/aws-sdk-go/service/s3"
 )
 
 var s3Svc *s3.S3
 
 func main() {
-	spBucket := flag.String("bucket", "", "Enter bucket to clear in-progress multipart uploads.")
+	spOp := flag.String("op", "removeMulti", "Enter operation to perform.")
+	spBucket := flag.String("bucket", "", "Enter bucket to perform operation on.")
+	spRegion := flag.String("region", "us-east-1", "The AWS Region to use for the bucket.")
 	flag.Parse()
 
-	config := aws.DefaultConfig
-
+	config := aws.NewConfig().WithRegion(*spRegion)
 	s3Svc = s3.New(config)
 
-	resp := mustListMultipartUploads(spBucket)
+	switch *spOp {
+	case "removeMulti":
+		resp := mustListMultipartUploads(spBucket)
 
-	fmt.Println(awsutil.StringValue(resp))
+		for _, upload := range resp.Uploads {
+			mustAbort(spBucket, upload)
+			confirmDeletion(spBucket, upload)
+		}
+	case "removeObjects":
+		params := &s3.ListObjectsInput{
+			Bucket:    spBucket, // Required
+			Delimiter: aws.String(""),
+			Prefix:    aws.String(""),
+		}
+		resp, err := s3Svc.ListObjects(params)
+		if err != nil {
+			if awsErr, ok := err.(awserr.Error); ok {
+				// Generic AWS Error with Code, Message, and original error (if any)
+				fmt.Println(awsErr.Code(), awsErr.Message(), awsErr.OrigErr())
+				if reqErr, ok := err.(awserr.RequestFailure); ok {
+					// A service error occurred
+					fmt.Println(reqErr.Code(), reqErr.Message(), reqErr.StatusCode(), reqErr.RequestID())
+				}
+			}
+		}
 
-	for _, upload := range resp.Uploads {
-		mustAbort(spBucket, upload)
-		confirmDeletion(spBucket, upload)
+		delparams := &s3.DeleteObjectsInput{
+			Bucket: spBucket, // Required
+			Delete: &s3.Delete{ // Required
+				Objects: make([]*s3.ObjectIdentifier, len(resp.Contents)),
+				Quiet:   aws.Bool(true),
+			},
+		}
+
+		for idx, obj := range resp.Contents {
+			delparams.Delete.Objects[idx] = &s3.ObjectIdentifier{
+				Key: obj.Key,
+			}
+		}
+
+		_, err = s3Svc.DeleteObjects(delparams)
+		if err != nil {
+			panic(err)
+		}
+	default:
+		fmt.Println("No operation supported.")
 	}
+
 }
 
 func mustListMultipartUploads(spBucket *string) *s3.ListMultipartUploadsOutput {
 	params := &s3.ListMultipartUploadsInput{
 		Bucket:     spBucket,
 		Delimiter:  aws.String(""),
-		MaxUploads: aws.Long(1000),
+		MaxUploads: aws.Int64(1000),
 		Prefix:     aws.String(""),
 	}
 
 	resp, err := s3Svc.ListMultipartUploads(params)
-	if awserr := aws.Error(err); awserr != nil {
-		log.Fatal("Error:", awserr.Code, awserr.Message)
-	} else if err != nil {
+	if err != nil {
 		panic(err)
 	}
 
@@ -52,35 +90,29 @@ func mustAbort(spBucket *string, upload *s3.MultipartUpload) {
 	params := &s3.AbortMultipartUploadInput{
 		Bucket:   spBucket,
 		Key:      upload.Key,
-		UploadID: upload.UploadID,
+		UploadId: upload.UploadId,
 	}
 
-	resp, err := s3Svc.AbortMultipartUpload(params)
-	if awserr := aws.Error(err); awserr != nil {
-		log.Fatal("Error:", awserr.Code, awserr.Message)
-	} else if err != nil {
+	_, err := s3Svc.AbortMultipartUpload(params)
+	if err != nil {
 		panic(err)
 	}
-
-	fmt.Println(awsutil.StringValue(resp))
 }
 
 func confirmDeletion(spBucket *string, upload *s3.MultipartUpload) {
 	params := &s3.ListPartsInput{
 		Bucket:   spBucket,
 		Key:      upload.Key,
-		UploadID: upload.UploadID,
-		MaxParts: aws.Long(1),
+		UploadId: upload.UploadId,
+		MaxParts: aws.Int64(1),
 	}
 
 	resp, err := s3Svc.ListParts(params)
-	if awserr := aws.Error(err); awserr != nil {
-		fmt.Println("Error:", awserr.Code, awserr.Message)
-		fmt.Println("The upload below still has Parts listed.")
-		fmt.Println(awsutil.StringValue(upload))
-	} else if err != nil {
-		panic(err)
+	if err != nil {
+		fmt.Println(err)
 	}
 
-	fmt.Println(awsutil.StringValue(resp))
+	if len(resp.Parts) > 0 {
+		fmt.Println("This upload still has parts in it.")
+	}
 }
